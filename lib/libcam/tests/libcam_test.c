3053d4b7ab06e92207292f51d48d811f4d1792f2
Evan Cramer <ecramer@ec-fbsd-current.west.isilon.com>
2016-04-07 00:45:49 -0700

    Adding new test case.

--- lib/libcam/tests/libcam_test.c
+++ lib/libcam/tests/libcam_test.c
@@ -33,9 +33,33 @@ ATF_TC_BODY(libcam_get_device_not_exists, tc)
 
 }
 
+ATF_TC(libcam_get_device_invalid_device);
+
+ATF_TC_HEAD(libcam_get_device_invalid_device, tc)
+{
+
+	atf_tc_set_md_var(tc, "descr", "test cam_get_device with a invalid device name.");
+}
+
+ATF_TC_BODY(libcam_get_device_invalid_device, tc)
+{
+	char* path = "/dev/foo1";
+	char  dev_name[10];
+	int   devnamelen = 10;
+	int   unit = 0;
+	int   result = 0;
+	struct cam_device * dev;
+
+	atf_tc_expect_fail("Passing in /dev/foo1, a bad device path.");
+
+	ATF_CHECK(cam_get_device(path, dev_name, devnamelen, &unit) == -1);
+
+}
+
 ATF_TP_ADD_TCS(tp)
 {
 	ATF_TP_ADD_TC(tp, libcam_get_device_not_exists);
+	ATF_TP_ADD_TC(tp, libcam_get_device_invalid_device);
 
 	return atf_no_error();
 }
